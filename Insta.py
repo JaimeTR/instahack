f5a2f99575299908c07e61e863e03044026b459a
Jaime Tarazona <38092883+JaimeTR@users.noreply.github.com>
2021-05-29 21:43:11 -0500

    Update Insta.py

--- Insta.py
+++ Insta.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 ###############################################################################
-# Instagram Brute Forcer
+# Instagram Brute Force
 # !/usr/bin/python
 ###############################################################################
 from __future__ import print_function
